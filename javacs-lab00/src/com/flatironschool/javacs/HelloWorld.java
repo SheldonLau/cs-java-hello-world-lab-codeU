96a38a912c716964d37541e67551d960b4dc15e7
SheldonLau <sheldonlau1@gmail.com>
2016-03-20 19:49:30 -0700

    Added code to get java version

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,12 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+	String version = System.getProperty("java.specification.version");
+	Double versionNum = Double.parseDouble(version);
+	return versionNum;
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	System.out.println("Java version is: " + getVersion());
     }
 }
